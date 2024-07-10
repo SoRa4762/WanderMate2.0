45aa912382c7bf06014714d52999db8ebdf17650
SoRa <sonamraisx007@gmail.com>
2024-07-10 21:33:55 +0545

    animations to side menu added

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,7 +1,90 @@
-const LandingPage = () => {
-  return (
-    <div>LandingPage</div>
-  )
-}
+import { useState } from "react";
+import { motion } from "framer-motion";
+import { RxHamburgerMenu } from "react-icons/rx";
+import BGImg from "../assets/bg8.jpg";
+import HeaderImage from "../assets/headerImg5.jpg";
+import { landingFooterLinks } from "../helper/data";
 
-export default LandingPage
+const LandingPage = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      {/* encapsulates all */}
+      <div
+        className="h-screen w-full flex flex-col justify-between"
+        style={{
+          background: `linear-gradient(rgba(0, 128, 128, 0.6), rgba(0, 128, 128, 0.6)),url(${BGImg})`,
+          backgroundSize: "cover",
+          backgroundPosition: "top",
+        }}
+      >
+        {/* header */}
+        <div className="h-[10vh] w-full pl-4 sm:pl-6 md:pl-8 flex justify-between items-center">
+          <RxHamburgerMenu
+            size={25}
+            color="white"
+            className="cursor-pointer sm:hidden"
+            onClick={() => {
+              setOpen(!open);
+            }}
+          />
+          <img
+            style={{
+              clipPath: "polygon(0 0, 100% 0, 100% 90%, 75% 100%, 0 50%)",
+            }}
+            className="h-52 w-40 object-cover"
+            src={HeaderImage}
+            alt="image"
+          />
+        </div>
+        {/* side menu */}
+        <motion.div
+          initial={{ x: open ? 400 : 0 }}
+          animate={{ x: open ? 0 : 400 }}
+          transition={{ duration: 0.3 }}
+          className={`
+            h-screen w-[80%] fixed top-0 right-0 transition-all duration-300 ease-linear bg-white z-50 rounded-lg`}
+        ></motion.div>
+        ){/* hero */}
+        <div className="w-full text-center text-white">
+          <p className="text-lg font-bold md:text-xl lg:text-2xl bg-gradient-to-b from-gray-400 to-gray-100 text-transparent bg-clip-text">
+            The Country of Himalays
+          </p>
+          <h1
+            className="font-bold text-7xl md:text-8xl lg:text-9xl"
+            style={{ textShadow: "4px 4px 8px rgba(0, 0, 0, 0.4)" }}
+          >
+            <span className="text-yellow-600">NEP</span>AL
+          </h1>
+        </div>
+        {/* footer */}
+        <div className="w-full grid grid-cols-2">
+          {/* first side */}
+          <div className="h-full w-full">
+            <p className="leading-6 pl-6 md:pl-12 pb-6 md:pb-12 text-white">
+              Visit Nepal, You will never regret it. <br /> This is something
+              incredible, fantastic, <br /> mesmerizing and lifetime experience.{" "}
+            </p>
+          </div>
+          {/* second side */}
+          <div className="h-full w-full flex items-end">
+            <div className="h-20 w-full flex gap-4 flex-wrap pl-2 backdrop-blur-sm">
+              {landingFooterLinks.map((link) => (
+                <>
+                  <li
+                    key={link.id}
+                    className="text-white list-none hover:border-b-4 border-yellow-400 "
+                  >
+                    <a href={link.link}>{link.title}</a>
+                  </li>
+                </>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LandingPage;
---
+++ src/helper/data.js
@@ -0,0 +1,27 @@
+export const landingFooterLinks = [
+  {
+    id: 1,
+    title: "Facebook",
+    link: "https://www.facebook.com",
+  },
+  {
+    id: 2,
+    title: "X",
+    link: "https://www.x.com",
+  },
+  {
+    id: 3,
+    title: "Instagram",
+    link: "https://www.instagram.com",
+  },
+  {
+    id: 4,
+    title: "Youtube",
+    link: "https://www.youtube.com",
+  },
+  {
+    id: 5,
+    title: "Google",
+    link: "https://www.google.com",
+  },
+];
