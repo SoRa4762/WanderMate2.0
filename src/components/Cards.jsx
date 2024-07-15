bdac9d73e6926432eb1f9af943c9f8c163b62047
SoRa <sonamraisx007@gmail.com>
2024-07-15 10:43:10 +0545

    Setting up Data

---
+++ src/components/Cards.jsx
@@ -0,0 +1,33 @@
+import { CiHeart } from "react-icons/ci";
+import { Link } from "react-router-dom";
+
+const Cards = ({ data }) => {
+  return (
+    <div className="h-full w-full grid grid-cols-2 xl:grid-cols-4 gap-4 pt-4">
+      {data?.map((item) => (
+        <>
+          <Link
+            to={`#`}
+            key={item.id}
+            className="h-[30vh] sm:h-[40vh] xl:h-[50vh] w-full flex flex-col justify-between bg-red-200 rounded-lg cursor-pointer drop-shadow-lg  hover:-translate-y-3 transition-all ease-in-out duration-300"
+            style={{
+              background: `url(${item.image[0]})`,
+              backgroundSize: "cover",
+            }}
+          >
+            {/* lg */}
+            <div className="h-8 w-8 m-2 rounded-full bg-white flex justify-center items-center">
+              <CiHeart size={24} />
+            </div>
+            <div className="backdrop-blur-sm text-xs sm:text-sm md:text-base rounded-b-lg p-3 sm:p-4 flex justify-between text-white font-bold">
+              <p>{item.name}</p>
+              <p>${item.price}</p>
+            </div>
+          </Link>
+        </>
+      ))}
+    </div>
+  );
+};
+
+export default Cards;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,110 @@
+import { hotels, travelPackages, topDestinations } from "../helper/data";
+// import { fetchHotels, fetchTravelPackages, fetchTopDestinations } from "../api";
+import Cards from "../components/Cards";
+import TopHotels from "../assets/bg2.jpg";
+import TopTravelPackage from "../assets/bg5.jpg";
+import TopDestination from "../assets/bg.jpg";
+import bgImg from "../assets/headerImg9.jpg";
+import { IoSearch } from "react-icons/io5";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 const Home = () => {
   return (
     <>
-      <Header />
-      Home
-      <Footer />
+      <div className="px-2 sm:px-4 md:px-8 lg:px-20">
+        <Header />
+
+        {/* search section */}
+        <div
+          className="h-[40vh] relative sm:h-[55vh] md:h-[70vh] lg:h-[75vh] mt-8 w-full transition-all duration-300 ease-in-out rounded-lg"
+          style={{
+            background: `url(${bgImg})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
+          <div className="h-[12%] lg:w-[39%] absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] bg-white flex justify-center items-center gap-2 p-1 md:p-4 rounded-lg">
+            <input
+              className="h-[90%] w-[80%] lg:w-[90%] border-none outline-none text-xs sm:text-sm md:text-base lg:text-lg"
+              placeholder="Search Your Places, Destination..."
+              type="text"
+            />
+            <div className="h-5 w-5 sm:h-7 sm:w-7 md:h-10 md:w-10 lg:h-11 lg:w-11 flex items-center justify-center bg-blue-600 rounded-full cursor-pointer">
+              <IoSearch
+                className="lg:flex xs:hidden hidden"
+                size={22}
+                color="white"
+              />
+              <IoSearch
+                className="md:flex lg:hidden hidden"
+                size={18}
+                color="white"
+              />
+              <IoSearch
+                className="sm:flex md:hidden hidden"
+                size={16}
+                color="white"
+              />
+              <IoSearch className="flex sm:hidden" size={12} color="white" />
+            </div>
+          </div>
+        </div>
+
+        <div className="h-full w-full flex flex-col">
+          {/* destinations */}
+          <div className="h-full w-full flex flex-col pt-4 xl:px-10">
+            {/* heading */}
+            <div className="flex gap-2 items-center pt-8">
+              <img
+                className="h-8 w-8 rounded-full cursor-pointer"
+                src={TopDestination}
+                alt="randomIcon"
+              />
+              <h1 className="font-bold text-md sm:text-lg lg:text-xl cursor-pointer">
+                Top Destinations
+              </h1>
+            </div>
+            {/* cards */}
+            <Cards data={topDestinations} url={destinationUrl} />
+          </div>
+
+          {/* hotels */}
+          <div className="h-full w-full flex flex-col pt-4 xl:px-10">
+            {/* heading */}
+            <div className="flex gap-2 items-center pt-8">
+              <img
+                className="h-8 w-8 rounded-full cursor-pointer"
+                src={TopHotels}
+                alt="randomIcon"
+              />
+              <h1 className="font-bold text-md sm:text-lg lg:text-xl cursor-pointer">
+                Top Hotels
+              </h1>
+            </div>
+            {/* cards */}
+            <Cards data={hotels} url={hotelUrl} />
+          </div>
+
+          {/* top travel packages */}
+          <div className="h-full w-full flex flex-col xl:px-10">
+            {/* heading */}
+            <div className="flex gap-2 items-center pt-8">
+              <img
+                className="h-8 w-8 rounded-full cursor-pointer"
+                src={TopTravelPackage}
+                alt="randomIcon"
+              />
+              <h1 className="font-bold text-md sm:text-lg lg:text-xl cursor-pointer">
+                Top Travel Packages
+              </h1>
+            </div>
+            {/* cards */}
+            <Cards data={travelPackages} />
+          </div>
+        </div>
+        <Footer />
+      </div>
     </>
   );
 };
